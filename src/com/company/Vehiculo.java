e096bd9dcdc3f301788b917d01ec6b9ae09f2044
Pichu <aristi.esteban@outlook.com>
2020-03-18 10:38:45 -0300

    Clase Vehiculo

---
+++ src/com/company/Vehiculo.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Vehiculo {
+
+    private Integer velocidad;
+    private Integer aceleracion;
+    private Double anguloDeGiro;
+    private String patente;
+    private Integer peso;
+    private Integer ruedas;
+
+}
